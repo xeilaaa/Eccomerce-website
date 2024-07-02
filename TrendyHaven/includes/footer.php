dd0132601703276327515695e3c0957637491666
xeilaaa <141280355+xeilaaa@users.noreply.github.com>
2024-07-02 12:42:47 +0200

    Delete TrendyHaven directory

--- TrendyHaven/includes/footer.php
+++
@@ -1,4 +0,0 @@
-<p>&nbsp;</p>
-<div class= "bg-info p-3 text-center ">
-<p>&copy; 2023. All rights reserverd </p>
-</div>
--- TrendyHaven/user_side/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-
-session_unset();
-session_destroy();
-echo "<script> window.open('../main.php','_self')</script>"
-?>
